d4cee06578555a9b624faa88c1c561db75ab3bd6
srikanthmuktala123 <srikanth.fresher68@gmail.com>
2024-04-29 00:47:55 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull srikanthfresher68842/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 srikanthfresher68842/simple-python-flask-app
+docker run -d -p 8000:8000 srikanthfresher68842/simple-python-flask-app
